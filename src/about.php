2ea654ba094852e92a4b52ee6e7b0690b157bd6a
JonathanMSifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2019-11-15 16:05:41 +0000

    Add greeting message

--- src/about.php
+++ src/about.php
@@ -6,7 +6,18 @@
 // execute the header script:
 require_once "header.php";
 
-echo "This is the skeleton code for 2CWK50. See the assignment specification for details of how you need to extend it.<br>You may wish to include a short description of your survey site and how to use the main features it has here.<br><br>";
+echo "You may wish to include a short description of your survey site and how to use the main features it has here.<br>";
+
+error_reporting(0);
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+error_reporting(1);
+
+if (! $connection) {
+    echo "<br>";
+    echo "You must first create the database before the site can be used";
+    echo "<br>";
+    echo "<a href = create_database.php> Initialise database </a>";
+}
 
 // finish of the HTML for this page:
 require_once "footer.php";
