56e39deae7191e2c9f399139f6d1be0f81994007
JonathanMSifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2019-11-20 15:55:51 +0000

    Improve database types

--- src/init_database.php
+++ src/init_database.php
@@ -102,7 +102,7 @@ function createSurveyTable($connection)
         die("Error checking for survey table: " . mysqli_error($connection));
     }
     // make our table:
-    $sql = "CREATE TABLE surveys (surveyID VARCHAR(32), username VARCHAR(16), title VARCHAR(64), instructions VARCHAR(500), numQuestions SMALLINT, topic VARCHAR(12), FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE, PRIMARY KEY (surveyID))";
+    $sql = "CREATE TABLE surveys (surveyID VARCHAR(32), username VARCHAR(20), title VARCHAR(64), instructions VARCHAR(500), numQuestions SMALLINT, topic VARCHAR(12), FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE, PRIMARY KEY (surveyID))";
     // no data returned, we just test for true(success)/false(failure):
     if (mysqli_query($connection, $sql)) {
         echo "Table created successfully: surveys<br>";
@@ -122,7 +122,7 @@ function createQuestionTable($connection)
         die("Error checking for existing table: " . mysqli_error($connection));
     }
     // make our table:
-    $sql = "CREATE TABLE questions (questionID VARCHAR(32), surveyID VARCHAR(32), questionNo VARCHAR(3), questionName VARCHAR(128), type VARCHAR(32), numOptions SMALLINT, required BOOLEAN, FOREIGN KEY (surveyID) REFERENCES surveys(surveyID) ON DELETE CASCADE, PRIMARY KEY (questionID))";
+    $sql = "CREATE TABLE questions (questionID VARCHAR(32), surveyID VARCHAR(32), questionNo TINYINT, questionName VARCHAR(128), type VARCHAR(32), numOptions SMALLINT, required BOOLEAN, FOREIGN KEY (surveyID) REFERENCES surveys(surveyID) ON DELETE CASCADE, PRIMARY KEY (questionID))";
     // no data returned, we just test for true(success)/false(failure):
     if (mysqli_query($connection, $sql)) {
         echo "Table created successfully: questions<br>";
