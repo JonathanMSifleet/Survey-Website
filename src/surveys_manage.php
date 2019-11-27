6b339f01826405cfae657e9e9038d5e4ce842e47
JonathanMSifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2019-11-27 14:25:06 +0000

    Let user view their own survey results

--- src/surveys_manage.php
+++ src/surveys_manage.php
@@ -79,9 +79,9 @@ function printSurveys($connection)
             echo "<tr><td>{$row['surveyID']}</td><td>{$row['username']}</td><td>{$row['title']}</td><td>{$row['topic']}</td><td><a href = answer_survey.php?surveyID={$row['surveyID']}&questionsAnswered=0>Survey link</a></td><td><a href = view_survey_results.php?surveyID={$row['surveyID']}>View Results</a><td><a href = ?deleteSurvey=true&surveyID={$row['surveyID']}> Delete</a></td></tr>";
         }
     } else {
-        echo "<tr><th>Survey ID</th><th>Title</th><th>Topic</th><th>Survey link</th><th>Delete survey</th></tr>";
+        echo "<tr><th>Survey ID</th><th>Title</th><th>Topic</th><th>Survey link</th><th>View results</th><th>Delete survey</th></tr>";
         while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr><td>{$row['surveyID']}</td><td>{$row['title']}</td><td>{$row['topic']}</td><td><a href = answer_survey.php?surveyID={$row['surveyID']}&questionsAnswered=0> Survey link</a></td><td><a href = ?deleteSurvey=true&surveyID={$row['surveyID']}> Delete</a></td></tr>";
+            echo "<tr><td>{$row['surveyID']}</td><td>{$row['title']}</td><td>{$row['topic']}</td><td><a href = answer_survey.php?surveyID={$row['surveyID']}&questionsAnswered=0> Survey link</a></td><td><a href = view_survey_results.php?surveyID={$row['surveyID']}>View Results</a></td><td><a href = ?deleteSurvey=true&surveyID={$row['surveyID']}> Delete</a></td></tr>";
         }
     }
     echo "</table>";
